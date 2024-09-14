5acc8857b8842f9b34522f22fa883f079b4ce2ad
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-09-14 08:02:08 +0530

    add main and preload files

---
+++ desktop-app/main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
+const { ipcMain } = require("electron/main");
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  const startUrl = url.format({
+    pathname: path.join(__dirname, "./app/build/index.html"),
+    protocol: "file",
+  });
+
+  mainWindow.loadURL(startUrl); // load the index.html file into the window
+
+  mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(createMainWindow);
