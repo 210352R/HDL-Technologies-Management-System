6ddd0b1e8802dc8857610d9fbaecc55c36b2a8a1
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-09-22 02:53:05 +0530

    add function to create bill with excisting laptop

--- server/services/billService.js
+++ server/services/billService.js
@@ -35,3 +35,35 @@ export const createNewBill = async (bill) => {
   });
   return { bill: newBill, qr_code: lap.qrcode };
 };
+
+// create bill for existing lap
+export const createBillForExistingLap = async (bill) => {
+  const {
+    lapId,
+    name,
+    phone,
+    address,
+    issue,
+    amount,
+    announce_date,
+    handover_date,
+    status,
+    images,
+  } = bill;
+  const userId = await createUser({ name, phone, address });
+  const date = new Date().toISOString(); // current date
+  const newBill = await prisma.bill.create({
+    data: {
+      lapId,
+      userId,
+      issue,
+      amount,
+      date,
+      announce_date,
+      handover_date,
+      status,
+      images,
+    },
+  });
+  return newBill;
+};
