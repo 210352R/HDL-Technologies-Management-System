e4fb3fa044cfeb13bd5317b8d9b0cfa5ad2ca143
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-09-22 02:39:38 +0530

    add new-bill endpoint

--- server/controllers/bill_controller.js
+++ server/controllers/bill_controller.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createLap } from "../services/lapService.js";
+import { createNewBill } from "../services/billService.js";
 
 export const bill_router = express.Router();
 
@@ -17,3 +18,38 @@ bill_router.post("/add-lap", async (req, res) => {
 });
 
 // create post method for add bill for new laptop
+bill_router.post("/add-new-bill", async (req, res) => {
+  const {
+    name,
+    phone,
+    address,
+    brand,
+    model,
+    issue,
+    amount,
+    announce_date,
+    handover_date,
+    status,
+    images,
+  } = req.body;
+  try {
+    const bill = {
+      name,
+      phone,
+      address,
+      brand,
+      model,
+      issue,
+      amount,
+      announce_date,
+      handover_date,
+      status,
+      images,
+    };
+    const newBill = await createNewBill(bill);
+    res.status(201).json({ success: true, qr_code: newBill.qr_code });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Bill could not be added" });
+  }
+});
