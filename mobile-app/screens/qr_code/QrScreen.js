8af85064b544f74572a92250a3445375011524f5
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-09-23 00:13:56 +0530

    add bck button to qr page

--- mobile-app/screens/qr_code/QrScreen.js
+++ mobile-app/screens/qr_code/QrScreen.js
@@ -1,9 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/MaterialIcons"; // Import the icon library
 
 const QrScreen = () => {
+  const navigation = useNavigation();
+
+  const handleBackPress = () => {
+    navigation.goBack();
+  };
+
   return (
     <View style={styles.container}>
+      <Icon
+        name="arrow-back"
+        size={30}
+        color="#000"
+        onPress={handleBackPress}
+        style={styles.backIcon}
+      />
       <Text>QrScreen</Text>
     </View>
   );
@@ -17,5 +32,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 20,
+  },
+  backIcon: {
+    position: "absolute",
+    top: 40,
+    left: 20,
   },
 });
