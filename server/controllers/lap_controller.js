e085656d12f1c445609f81b70cd86e5800019fc7
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-09-23 16:52:21 +0530

    add get billss get end point

---
+++ server/controllers/lap_controller.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getBillDetails } from "../services/lapService";
+
+export const lap_router = express.Router();
+
+// create get route for get related bills by lap id
+lap_router.get("/get-bill/:lapId", async (req, res) => {
+  const { lapId } = req.params;
+  try {
+    const bills = await getBillDetails(lapId);
+    res.status(200).json(bills);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: "Bills could not be fetched" });
+  }
+});
