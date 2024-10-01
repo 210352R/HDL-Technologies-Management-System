22d61909b1acfe296101c53e190c67c88fecb92c
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-02 02:39:58 +0530

    update category details button

--- mobile-app/screens/home/CategoryButtons.js
+++ mobile-app/screens/home/CategoryButtons.js
@@ -7,16 +7,28 @@ import {
   ScrollView,
 } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome"; // Change to any icon set you prefer
+import { useNavigation } from "@react-navigation/native";
 
 const categories = [
-  { name: "Analytics", icon: "bar-chart", color: "#3F51B5" },
-  { name: "Customers", icon: "home", color: "#FF9800" },
-  { name: "Orders", icon: "file-text", color: "#E91E63" },
-  { name: "Tasks", icon: "tasks", color: "#4CAF50" },
-  { name: "Sales", icon: "bell", color: "#FFEB3B" },
+  {
+    name: "Analytics",
+    icon: "bar-chart",
+    color: "#3F51B5",
+    link: "/AnalyticsScreen",
+  },
+  { name: "Customers", icon: "home", color: "#FF9800", link: "/Users" },
+  { name: "Orders", icon: "file-text", color: "#E91E63", link: "/Bills" },
+  { name: "Laps", icon: "tasks", color: "#4CAF50", link: "/Laps" },
+  { name: "Sales", icon: "bell", color: "#FFEB3B", link: "/Sales" },
 ];
 
 const CategoryButtons = () => {
+  const navigation = useNavigation(); // Hook to get the navigation prop
+
+  const handlePress = (categoryLink) => {
+    navigation.navigate(categoryLink); // Navigate to the screen with the corresponding name
+  };
+
   return (
     <ScrollView
       horizontal
@@ -26,6 +38,7 @@ const CategoryButtons = () => {
       {categories.map((category, index) => (
         <TouchableOpacity
           key={index}
+          onPress={() => handlePress(category.link)}
           style={[styles.button, { borderColor: category.color }]}
         >
           <Icon name={category.icon} size={30} color={category.color} />
