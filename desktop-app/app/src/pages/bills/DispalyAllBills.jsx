2bb4c5ed6d3ffdb814db0e5cb85a957b113116c4
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-10-11 01:21:18 +0530

    update display bills page

--- desktop-app/app/src/pages/bills/DispalyAllBills.jsx
+++ desktop-app/app/src/pages/bills/DispalyAllBills.jsx
@@ -129,31 +129,56 @@ const BillList = () => {
         </div>
 
         {/* Popup for bill details */}
-        {isPopupOpen && (
+        {isPopupOpen && selectedBill && (
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white dark:bg-gray-800 dark:text-white rounded-lg p-6 w-11/12 md:w-1/2 lg:w-1/3">
               <h2 className="text-xl font-semibold mb-4">
-                Bill Details for ID: {selectedBill?.billId}
+                Bill Details for ID: {selectedBill.billId}
               </h2>
               <p>
-                <strong>Lap Brand:</strong> {selectedBill?.lap.lap_brand}
+                <strong>Lap Brand:</strong> {selectedBill.lap.brand}
               </p>
               <p>
-                <strong>Lap Model:</strong> {selectedBill?.lap.lap_model}
+                <strong>Lap Model:</strong> {selectedBill.lap.model}
               </p>
               <p>
-                <strong>Issue:</strong> {selectedBill?.issue}
+                <strong>RAM:</strong> {selectedBill.lap.ram}
               </p>
               <p>
-                <strong>Status:</strong> {selectedBill?.status}
+                <strong>SSD:</strong> {selectedBill.lap.ssd}
               </p>
               <p>
-                <strong>Announce Date:</strong> {selectedBill?.announce_date}
+                <strong>Hard Disk:</strong> {selectedBill.lap.hard}
+              </p>
+              <p>
+                <strong>Issue:</strong> {selectedBill.issue}
+              </p>
+              <p>
+                <strong>Status:</strong> {selectedBill.status}
+              </p>
+              <p>
+                <strong>Amount:</strong> ${selectedBill.amount}
+              </p>
+              <p>
+                <strong>Announce Date:</strong> {selectedBill.announce_date}
               </p>
               <p>
                 <strong>Handover Date:</strong>{" "}
-                {selectedBill?.handover_date || "Pending"}
+                {selectedBill.handover_date || "Pending"}
               </p>
+
+              <h3 className="font-semibold mt-4">Images:</h3>
+              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-2">
+                {selectedBill.images.map((image, index) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt={`Bill Image ${index + 1}`}
+                    className="rounded shadow"
+                  />
+                ))}
+              </div>
+
               <button
                 onClick={closePopup}
                 className="mt-4 bg-blue-500 text-white rounded px-4 py-2"
