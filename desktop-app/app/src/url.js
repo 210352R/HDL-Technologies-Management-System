ec9617284fc7c9bd30f1fd3feb2a3f2f1b55f381
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2024-12-31 21:10:44 +0530

    add https uri from aws

--- desktop-app/app/src/url.js
+++ desktop-app/app/src/url.js
@@ -3,4 +3,6 @@
 // export const url = "https://hdl-technologies-management-system.vercel.app";
 
 // change to localhost for development
-export const url = "http://localhost:8000";
+// export const url = "http://localhost:8000";
+
+export const url = "https://fkekyfys8i.execute-api.eu-north-1.amazonaws.com";
